f9114fc7f92a00325fcf6261966891b7d553537d
drjcoddington <drjcoddington@yahoo.com>
2024-11-04 13:02:23 -0700

    Add files via upload

---
+++ Fall week 4/sound.js
@@ -0,0 +1,19 @@
+function sound(src)
+{// add play function
+    this.sound = document.createElement("audio") ;//create audio
+    this.sound.src = src;//define src
+    this.play = function() {//music function
+        this.sound.play();//play sound
+    }
+}
+function play()
+
+{
+    alert("func")
+    mySound = new sound("sounds for class/wind.mp3") // this plays the sound with the buttons to start and stop//
+    mySound.play();
+}
+function stop()
+{
+    window.location.reload()//this stops the sound and once started again it will start over// 
+}
